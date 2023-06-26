72b63e981728247bdd0206e2f7fdd40461d33dd7
Dimael Rivas <artrivas@pop-os.localdomain>
2023-06-25 19:05:05 -0500

    cambios

---
+++ lab6/segundaparte/archivos/condlogic.v
@@ -0,0 +1,66 @@
+// reuse code from prior labs.
+module condlogic (
+	clk,
+	reset,
+	Cond,
+	ALUFlags,
+	FlagW,
+	PCS,
+	NextPC,
+	RegW,
+	MemW,
+	PCWrite,
+	RegWrite,
+	MemWrite
+);
+	input wire clk;
+	input wire reset;
+	input wire [3:0] Cond;
+	input wire [3:0] ALUFlags;
+	input wire [1:0] FlagW;
+	input wire PCS;
+	input wire NextPC;
+	input wire RegW;
+	input wire MemW;
+	output wire PCWrite;
+	output wire RegWrite;
+	output wire MemWrite;
+	wire [1:0] FlagWrite;
+	wire [3:0] Flags;
+	wire CondEx;
+    wire AfterCondEx;
+
+	// Delay writing flags until ALUWB state
+	flopr #(1) flagwritereg(
+		clk,
+		reset,
+		CondEx,
+		AfterCondEx
+	);
+
+	// ADD CODE HERE
+	flopenr #(2) flagreg1(
+		.clk(clk),
+		.reset(reset),
+		.en(FlagWrite[1]),
+		.d(ALUFlags[3:2]),
+		.q(Flags[3:2])
+	);
+	flopenr #(2) flagreg0(
+		.clk(clk),
+		.reset(reset),
+		.en(FlagWrite[0]),
+		.d(ALUFlags[1:0]),
+		.q(Flags[1:0])
+	);
+	condcheck cc(
+		.Cond(Cond),
+		.Flags(Flags),
+		.CondEx(CondEx)
+	);
+	assign PCWrite = NextPC | (AfterCondEx & PCS);
+	assign RegWrite = AfterCondEx & RegW;
+	assign MemWrite = AfterCondEx & MemW;
+    assign FlagWrite = FlagW & {2 {CondEx}};
+
+endmodule
